7d22024e818927e07e65504a23f8540cbd73b2c3
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-25 01:06:09 +0300

    Delete UserRepo.java

--- UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Java.Bug.Tracker.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Java.Bug.Tracker.Models.User;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-
-}
