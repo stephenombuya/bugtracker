c4479e1137fe128663a58637c46c9727824b6070
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-25 01:05:21 +0300

    Delete ProjectRepo.java

--- ProjectRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Java.Bug.Tracker.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Java.Bug.Tracker.Models.Project;
-
-public interface ProjectRepo extends JpaRepository<Project, Integer>{
-
-}
